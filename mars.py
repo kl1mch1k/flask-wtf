2b3cd692d8cfd1890f23b5a951bc3da4563fc64b
kl1mch1k <klimov.lenar@yandex.ru>
2022-03-19 15:52:39 +0300

    3

--- mars.py
+++ mars.py
@@ -14,5 +14,16 @@ def training(prof):
     return render_template('base.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    params = {}
+    params['list'] = list
+    params['profs'] = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                       'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите', 'астрогеолог',
+                       'гляциолог', 'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                       'штурман', 'пилот дронов']
+    return render_template('base.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
